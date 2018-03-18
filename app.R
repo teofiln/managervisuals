268b317e364925b212f1fc71fbc5e295d8386f17
novica <nnovica@gmail.com>
2018-03-18 12:13:22 +0100

    remove data clean up from app

--- app.R
+++ app.R
@@ -43,40 +43,10 @@ server <- function(input, output) {
     
   })
   
-  cash_summary <- function(df.raw){
-    df.raw <- read.delim(getFile()$datapath, sep=getSeparator(), stringsAsFactors = FALSE) 
-    df.raw$Date <- gsub("X", "", colnames(df.raw)[3])
-    # replace the name of column 3, which was the date, with 'amount'
-    colnames(df.raw)[3] <- "Amount"
-    # replace te name of column 2 with 'account'
-    colnames(df.raw)[2] <- "Account"
-    # make sure numbers are formated with . for decimal
-    df.raw$Amount <- gsub("\\.", "", df.raw$Amount)
-    df.raw$Amount <- gsub(",", ".", df.raw$Amount)
-    # make sure ammounts are numeric
-    df.raw$Amount <- as.numeric(df.raw$Amount)
-    #remove accounts with amount 0
-    df.raw <- filter(df.raw, Amount != 0)
-    
-    # the operation just subsets with the range from the total number of rows -4 
-    # to the total number of rows to take just the last four rows
-    df.raw_sums <- df.raw[(nrow(df.raw)-3):nrow(df.raw),]
-    
-    # reverse this subsetting with the minus sign
-    df.raw <- df.raw[-((nrow(df.raw)-3):nrow(df.raw)),]
-    
-    #make outflows negative (in case we need that)
-    df.raw <- df.raw %>%
-      mutate(Direction = case_when(Group == "Inflows" ~ Amount,
-                                   Group == "Outflows" ~ -Amount))
-    return(df.raw)
-  }
-  
-  
-  
   
   loadData <- reactive({
-    cash_summary(csvfile)
+    csvfile <- read.delim(getFile()$datapath, sep=getSeparator(), stringsAsFactors = FALSE) 
+    return(csvfile)
   })
   
   makePlot <- reactive({
